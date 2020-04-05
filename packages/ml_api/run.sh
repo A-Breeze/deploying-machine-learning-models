7d8c81993b5423848701a4196bfad4a2309b7b11
A-Breeze <alexander.breeze@yahoo.com>
2020-04-05 11:56:40 +0100

    test_docker: Update gunicorn command to start server, and add comments

--- packages/ml_api/run.sh
+++ packages/ml_api/run.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
+# Set the following environment variable
 export IS_DEBUG=${DEBUG:-false}
-exec gunicorn -b :${PORT:-5000} --access-logfile - --error-logfile - run:application
+# The syntax "${parameter:-word}" means: 
+# - If "parameter" exists (and not null), then return its value
+# - If "parameter" is not set (or null), then return "word" (or the expansion of it, if it is an expression)
+# See: <https://unix.stackexchange.com/a/122848>
+
+# Explanation of options:
+# -b  (i.e. --bind) => Socket to bind to (i.e. )
+# --access-logfile -  => log to stdout
+# --error-logfile -  => similarly for error logs
+# See: <https://docs.gunicorn.org/en/stable/settings.html>
+exec gunicorn -b :${PORT:-5000} --pythonpath packages/ml_api --access-logfile - --error-logfile - run:application
