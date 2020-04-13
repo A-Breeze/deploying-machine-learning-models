4732b0e50b645f7f99d099652d3a5efdf12a43d6
A-Breeze <alexander.breeze@yahoo.com>
2020-04-13 21:12:16 +0100

    Sect13_NN_model: CI config: Add debugging lines to sh script to fetch NN model package

--- scripts/fetch_kaggle_nn_package.sh
+++ scripts/fetch_kaggle_nn_package.sh
@@ -2,8 +2,14 @@
 
 kaggle datasets download -d btw78jt/neural-network-package-repo -p packages/neural_network_model/dist/ --unzip
 cd packages/neural_network_model/dist/
+ls -a
 mv neural_network_model-* neural_network_model
+ls -a
 cd neural_network_model
+ls -a
 mv * ..
+ls -a
 cd ..
+ls -a
 rm -rf neural_network_model
+ls -a
