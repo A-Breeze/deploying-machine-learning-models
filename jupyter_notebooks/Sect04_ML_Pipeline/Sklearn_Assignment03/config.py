1fab5fb7fda865da7f4b346358a51c315ab2739e
A-Breeze <alexander.breeze@yahoo.com>
2020-04-28 18:50:51 +0100

    Sect04_extra: Add Assignment 3 files for completing

---
+++ jupyter_notebooks/Sect04_ML_Pipeline/Sklearn_Assignment03/config.py
@@ -0,0 +1,20 @@
+"""Configuration variables for pipeline"""
+
+# ==== SETUP ====
+# Import modules
+from pathlib import Path
+
+
+# ==== PATHS ====
+TRAINING_DATA_FILE = "titanic.csv"
+PIPELINE_NAME = str(Path('Modelling_outputs') / 'lasso_regression')
+
+
+# ==== FEATURE GROUPS ====
+TARGET = 
+
+CATEGORICAL_VARS = 
+
+NUMERICAL_VARS = 
+
+CABIN = 
